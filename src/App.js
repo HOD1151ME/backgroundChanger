a8d4003e3f87930480aa237e2d7c60d4550c5ba0
Gajanan <gkadam875@gmail.com>
2024-03-18 00:51:48 +0530

    second commt

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   
   return (
    <>
+   <h1>bg color</h1>
     <div className='w-full h-screen duration-200'  style={{backgroundColor : color}}>
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl'>
